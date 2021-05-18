9c6b67b70358e20f6b6f52db511082a095c4fed3
mortimere-uu <mortimer-e@ulster.ac.uk>
2021-05-18 18:47:40 +0100

    Update EGM_assignment_script_population.py
    various changes to script to be able to generate county and population statistcs. Some issues with the maps need to be resolved.

--- EGM_assignment_script_population.py
+++ EGM_assignment_script_population.py
@@ -4,6 +4,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 import pandas as pd
 
 # generate matplotlib handles to create a legend of the features we put in our map.
@@ -14,22 +15,19 @@ def generate_handles(labels, colors, edge='k', alpha=1):
         handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
     return handles
 
+# add scale bar for maps
+
+
+# add template for graphs
 
 # plt.ion()
 
 # ---------------------------------------------------------------------------------------------------------------------
-# in this section, write the script to load the data and complete the main part of the analysis.
-# load datasets
-counties_UA = gpd.read_file (r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\data_files\Counties_and_Unitary_Authorities.shp')
-population_df = pd.read_csv(r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\data_files\population_number.csv') # uk population data from 1991 to 2019
 
-
-# try to print the results to the screen using the format method demonstrated in the workbook
-# print(counties.head())
-
-# load the necessary data here and transform to a UTM projection
-# counties = counties.to_crs(epsg=32629) # convert counties to UTM
-# wards = wards.to_crs(epsg=32629) # convert wards to UTM
+# load datasets to be used in the analysis
+countries = gpd.read_file(r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\data_files\Countries.shp') # 2020 UK country shapefile
+counties_UA = gpd.read_file (r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\data_files\Counties_and_Unitary_Authorities.shp') # 2020 UK Counties and Unitary Authorities shapefile
+population_df = pd.read_csv(r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\data_files\population_number.csv') # UK population data from 1991 to 2019
 
 # next step merge population data table with counties_ua shapefile
 
@@ -39,8 +37,63 @@ counties_UA_population = (pd.merge(counties_UA, population_df, on='CTYUA20CD', h
 del counties_UA_population['CTYUA20NMW'], counties_UA_population['BNG_E'], counties_UA_population['BNG_N'], counties_UA_population['LONG'], counties_UA_population['LAT'] # remove unwanted columns
 # print(counties_UA_population.columns) # column headers from merged table
 print(counties_UA_population)
+counties_UA_population.to_csv(r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\counties_UA_population.csv') # file note accurate
 
-# your analysis goes here...
+# mean_pop = (counties_UA_population['2019'].mean()) # calcualate mean counties_UA population for given year
+# max_pop = (counties_UA_population['2019'].max()) # calcualate max counties_UA population for given year how name county?
+# min_pop = (counties_UA_population['2019'].min()) # calcualate min counties_UA population for given year, how name county?
+# total_pop = (counties_UA_population['2019'].sum()) # totals the poulation for the given year
+# print('The mean county population for 2019 is {:.2f}.'.format(mean_pop)) # prints the mean counties_UA population for given year
+# print('The maximum county population for 2019 is {:}.'.format(max_pop)) # prints the max counties_UA population for given year
+# print('The minimum county population for 2019 is {:}.'.format(min_pop)) # prints the min counties_UA population for given year
+# print('The UK population for 2019 is {:}.'.format(total_pop)) # prints the total population for the given year
+
+def year_population(x): # make docstring
+    x = str(x)
+    mean_pop = (counties_UA_population[x].mean()) # this is wrong, not running on x running on 2019 hardwired?
+    print('The mean county population in the year ' + str(x) + ' was {:,.2f}.'.format(mean_pop))
+    max_pop = (counties_UA_population[x].max())
+    print('The maximum county population in the year ' + str(x) + ' was {:,}.'.format(max_pop))
+    min_pop = (counties_UA_population[x].min())
+    print('The minimum county population in the year ' + str(x) + ' was {:,}.'.format(min_pop))
+    total_pop = (counties_UA_population[x].sum())
+    print('The UK population in the year ' + str(x) + ' was {:,}.'.format(total_pop))
+
+year_population(2019)
+print()
+year_population(2004)
+print()
+year_population(1991)
+
+test_datasort = counties_UA_population[['County / unitary (as of April 2021)', '2019']] # selects the county/UA name column and population data for selected year
+print(test_datasort.sort_values(['2019'], ascending=[False])) # rearranges given year data into descending order (highest to lowest populated counties/UA.
+
+test_county_select = (counties_UA_population[counties_UA_population['County / unitary (as of April 2021)'] == 'Kent']) # selects a neamed county/UA row and all the population data
+print(test_county_select) # prints selected row
+
+# write a function to provide a list of Counties/UA
+def county_select(y):# for a given county select all the population data fom 1991 to 2019, make docstring
+    test = (counties_UA_population[counties_UA_population['County / unitary (as of April 2021)'] == (y)])
+    test.to_csv(r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\county_select_pop.csv') # want to save an output file with county name in it
+    print(test)
+
+
+county_select('Kent')
+county_select('Conwy')
+county_select('Devon')
+# print polygon features (practical 3?)
+
+test_county_select = (counties_UA_population[counties_UA_population['County / unitary (as of April 2021)'] == 'Kent']) # selects a neamed county/UA row and all the population data
+print(test_county_select) # prints selected row
+
+print(test_county_select.columns)
+
+del test_county_select['OBJECTID'], test_county_select['CTYUA20CD'], test_county_select['CTYUA20NM'], test_county_select['Shape__Are'], test_county_select['Shape__Len'], test_county_select['geometry'], test_county_select['County / unitary (as of April 2021)']
+
+print(test_county_select.columns)
+
+
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # below here, you may need to modify the script somewhat to create your map.
@@ -56,25 +109,26 @@ gridlines = ax.gridlines(draw_labels=True,
 gridlines.right_labels = False
 gridlines.bottom_labels = False
 
-
 # to make a nice colorbar that stays in line with our map, use these lines:
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
 # plot the ward data into our axis, using
-counties_UA_population_plot = counties_UA_population.plot(column='1991', ax=ax, vmin=2000, vmax=1600000, cmap='viridis',
-                       legend=True, cax=cax)
+counties_UA_population_plot = counties_UA_population.plot(column='2019', ax=ax, vmin=1800, vmax=1800000, cmap='viridis',
+                       legend=True, cax=cax, legend_kwds={'label' : 'Population', 'orientation': 'vertical'})
 
 counties_UA_outline = ShapelyFeature(counties_UA['geometry'], myCRS, edgecolor='k', facecolor='none', linewidth=0.2) # changed facecover
 ax.add_feature(counties_UA_outline)
 
 county_handles = generate_handles([''], ['none'], edge='k')
 
-ax.legend(county_handles, ['Counties'], fontsize=12, loc='upper left', framealpha=1)
+ax.legend(county_handles, ['Counties'], fontsize=10, loc='upper left', framealpha=1)
+
+# scale_bar(ax)
 
 # save the figure
 # plt.show()
-fig.savefig('sample_pop_newscript1991.png', dpi=300, bbox_inches='tight')
+# fig.savefig('sample_pop_newscript2019.png', dpi=300, bbox_inches='tight')
 
 
 # plot a line graph of data
