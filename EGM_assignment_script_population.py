e88d71165fdffd3fab1586e76cb883f3897e7943
mortimere-uu <mortimer-e@ulster.ac.uk>
2021-05-19 11:41:37 +0100

    Update EGM_assignment_script_population.py
    issues with scale and gridlines. think projection is incorrect

--- EGM_assignment_script_population.py
+++ EGM_assignment_script_population.py
@@ -17,23 +17,23 @@ def generate_handles(labels, colors, edge='k', alpha=1):
 
 # add scale bar for maps
 # create a scale bar of length 20 km in the upper right corner of the map - needs correcting
-def scale_bar(ax, location=(0.92, 0.95)):
-    llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
-    sbllx = (llx1 + llx0) / 2
-    sblly = lly0 + (lly1 - lly0) * location[1]
+# def scale_bar(ax, location=(0.92, 0.95)):
+#    llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
+#    sbllx = (llx1 + llx0) / 2
+#    sblly = lly0 + (lly1 - lly0) * location[1]
 
-    tmc = ccrs.TransverseMercator(sbllx, sblly)
-    x0, x1, y0, y1 = ax.get_extent(tmc)
-    sbx = x0 + (x1 - x0) * location[0]
-    sby = y0 + (y1 - y0) * location[1]
+#    tmc = ccrs.TransverseMercator(sbllx, sblly)
+#    x0, x1, y0, y1 = ax.get_extent(tmc)
+#    sbx = x0 + (x1 - x0) * location[0]
+#    sby = y0 + (y1 - y0) * location[1]
 
-    plt.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=tmc)
-    plt.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=tmc)
-    plt.plot([sbx-10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=tmc)
+#    plt.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=tmc)
+#    plt.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=tmc)
+#    plt.plot([sbx-10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=tmc)
 
-    plt.text(sbx, sby-4500, '20 km', transform=tmc, fontsize=8)
-    plt.text(sbx-12500, sby-4500, '10 km', transform=tmc, fontsize=8)
-    plt.text(sbx-24500, sby-4500, '0 km', transform=tmc, fontsize=8)
+#    plt.text(sbx, sby-4500, '20 km', transform=tmc, fontsize=8)
+#    plt.text(sbx-12500, sby-4500, '10 km', transform=tmc, fontsize=8)
+#    plt.text(sbx-24500, sby-4500, '0 km', transform=tmc, fontsize=8)
 
 # add template for graphs
 
@@ -46,6 +46,7 @@ countries = gpd.read_file(r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\data_
 counties_UA = gpd.read_file (r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\data_files\Counties_and_Unitary_Authorities.shp') # 2020 UK Counties and Unitary Authorities shapefile
 population_df = pd.read_csv(r'C:\Users\Ed\Documents\GitHub\EGM722_Assignment\data_files\population_number.csv') # UK population data from 1991 to 2019
 
+print(countries.crs)
 # next step merge population data table with counties_ua shapefile
 
 years = [str(yr) for yr in range(1991, 2020)]
@@ -122,7 +123,7 @@ print(counties_UA_population.sort_values(['Population change'], ascending=[False
 myCRS = ccrs.UTM(30)
 # create a figure of size 10x10 (representing the page size in inches
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
-ax = plt.axes(projection=ccrs.Mercator()) # create an axes object in the figure, using Mercater projection to plot data
+# ax = plt.axes(projection=ccrs.Mercator()) # create an axes object in the figure, using Mercater projection to plot data
 
 # add gridlines below
 gridlines = ax.gridlines(draw_labels=True,
@@ -147,7 +148,7 @@ county_handles = generate_handles([''], ['none'], edge='k')
 ax.legend(county_handles, ['Counties'], fontsize=10, loc='upper left', framealpha=1)
 # add year title to map/legend
 
-scale_bar(ax)
+# scale_bar(ax)
 
 # save the figure
 # plt.show()
